d58decd98b0b0ebef57624efac714e0250451f03
anomc-2000 <serraomarcos54@gmail.com>
2026-05-07 11:26:29 -0300

    feat: adição do Header e adição de assets
    * adição do header
    
    * modificação
    
    * correções 2#
    
    ---------
    
    Co-authored-by: Rafael Tamy <rafaeltamydb@gmail.com>

---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import { FaMagnifyingGlass, FaHeart, FaCartShopping } from "react-icons/fa6";
+import { FaRegUser } from "react-icons/fa";
+
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-[#B5DCFD]">
+      <div className="flex items-center justify-between px-6 py-2">
+
+       
+        <a href="#">
+          <img
+            src="src/assets/DANIELE.4mdp.png"
+            alt="Vul-pix"
+            width={200}
+            height={100}
+            className="ml-6"
+          />
+        </a>
+
+        
+        <div className="group relative flex items-center w-[60%] mx-auto px-4 py-1.5 bg-[#E0EEFF] border border-[#E0EEFF] rounded-full shadow-sm hover:shadow-md transition-shadow duration-300">
+          <input
+            className="w-full bg-transparent border-none outline-none text-base"
+            type="search"
+            placeholder="Buscar na loja"
+            aria-label="Search"
+          />
+          <FaMagnifyingGlass className="ml-2.5 text-lg text-[#0072C4] cursor-pointer" />
+        </div>
+
+        
+        <div className="flex items-center gap-6 mr-6">
+          <a href="#" aria-current="page">
+            <FaHeart className="text-xl text-[#0072C4]" />
+          </a>
+          <a href="#">
+            <FaCartShopping className="text-xl text-[#0072C4]" />
+          </a>
+          <a href="#">
+            <FaRegUser className="text-xl text-[#0072C4]" />
+          </a>
+        </div>
+
+      </div>
+    </nav>
+  );
+}
